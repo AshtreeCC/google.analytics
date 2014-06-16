294ef671cbf1f862d78321782ff7e9456ad81eae
Andrew Nash <akahadaka@gmail.com>
2014-06-16 19:11:33 +0200

    Initial commit

---
+++ google.analytics.class.php
@@ -0,0 +1,22 @@
+<?php
+
+class Plugin_Google_Analytics() {
+	
+	/**
+	 * Pastes analytics code at the bottom of the page
+	 */
+	public static function Activate($code) {
+		$htm = Ashtree_Html_Page::instance();
+		$htm->javascript = <<<JAVASCRIPT
+		var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', '{$code}']);
+		  _gaq.push(['_trackPageview']);
+	
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+JAVASCRIPT;
+	}
+}
